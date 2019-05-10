ed5d8a84f09a4baff0cfc3b3676c41b710d2ef11
Nolan Tiffney <nolantiffney@gmail.com>
2019-05-09 20:19:20 -0400

    Finished chapter 7.

--- main.hs
+++ main.hs
@@ -13,13 +13,27 @@ import qualified Data.Text.IO as TIO
 import Control.Monad
 import System.Random
 
-color :: Hitable a => Ray -> HitableList a -> Vect3
+randomInUnitSphere :: IO Vect3
+randomInUnitSphere = do
+  x <- randomRIO (0.0, 0.9999999999999999)
+  y <- randomRIO (0.0, 0.9999999999999999)
+  z <- randomRIO (0.0, 0.9999999999999999)
+  let p = mO (*2) (x,y,z) - (1,1,1)
+  if sqrLen p >= 1.0
+     then randomInUnitSphere
+     else return p
+
+color :: Hitable a => Ray -> HitableList a -> IO Vect3
 color r@(MkRay origin direction) world =
-  case hitL world r 0.0 9.9e999 of
+  case hitL world r 0.001 9.9e999 of
        Nothing -> let unitDir = unit direction
                       t = 0.5 * (getY unitDir) + 1.0 in
-                      mO (*(1.0-t)) (1.0, 1.0, 1.0) + mO (*t) (0.5, 0.7, 1.0)
-       Just (MkHR t p normal) -> mO (\x -> 0.5 * (x+1)) normal
+                      return $ mO (*(1.0-t)) (1.0, 1.0, 1.0) + mO (*t) (0.5, 0.7, 1.0)
+       Just (MkHR t p normal) -> do
+         rus <- randomInUnitSphere
+         let target = p + normal + rus
+         ci <- color (MkRay p (target - p)) world
+         return $ mO (*0.5) ci
 
 printVect :: Vect3 -> T.Text
 printVect v = let (x,y,z) = mO floor v in
@@ -28,12 +42,13 @@ printVect v = let (x,y,z) = mO floor v in
 getSamples :: Hitable a => Int -> Int -> Int -> Int -> Camera -> HitableList a ->
               Vect3 -> Int ->  IO Vect3
 getSamples i j nx ny camera world col a = do
-  r1 <- randomRIO (0.0, 1.0)
-  r2 <- randomRIO (0.0, 1.0)
+  r1 <- randomRIO (0.0, 0.9999999999999999)
+  r2 <- randomRIO (0.0, 0.9999999999999999)
   let u = (fromIntegral i + r1) / (fromIntegral nx)
       v = (fromIntegral j + r2) / (fromIntegral ny)
       r = getRay camera u v
-  return (col + color r world)
+  c1 <- color r world
+  return (col + c1)
 
 main :: IO ()
 main =
@@ -46,4 +61,4 @@ main =
       forM_ header TIO.putStrLn
       forM_ [(j,i) | j <- [(ny-1),(ny-2)..0], i <- [0..(nx-1)]] $ \(j,i) -> do
          col <- foldM (getSamples i j nx ny camera world) (0, 0, 0) [0..(ns-1)]
-         TIO.putStrLn $ printVect $ mO (\x -> (*) 255.99 $ x / (fromIntegral ns)) col
+         TIO.putStrLn $ printVect $ mO (\x -> (*) 255.99 $ sqrt $ x / (fromIntegral ns)) col
